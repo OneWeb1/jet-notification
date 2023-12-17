13f0bf519ae987d18dc346605300db2e7237ae97
AlickSadler <alicksadler@gmail.com>
2023-12-17 21:34:19 +0200

    'new'

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -15,6 +15,7 @@ const scrapeLogic = async res => {
 			process.env.NODE_ENV === 'production'
 				? process.env.PUPPETEER_EXECUTABLE_PATH
 				: puppeteer.executablePath(),
+		headless: 'new',
 	});
 	try {
 		const page = await browser.newPage();
